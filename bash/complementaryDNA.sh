9b3ae585b1768429bd035aefeb4c006272c0c82a
Mitterpach <patrik@mitterpach.com>
2022-01-23 23:53:40 +0100

    Finish complementaryDNA.sh

---
+++ bash/complementaryDNA.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G".
+#  You function receives one side of the DNA (string, except for Haskell);
+#  you need to return the other complementary side. DNA strand is never empty
+#  or there is no DNA at all (again, except for Haskell).
+
+DNA=$1
+echo $DNA | tr "ATGC" "TACG" 
